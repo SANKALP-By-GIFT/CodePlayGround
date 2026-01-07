e89d4ef78a792bab52c34bdc640313c5c0aaf1ea
Biswajit Sahoo <bjsahoo199@gmail.com>
2026-01-07 15:26:39 +0530

    ep02 Updated

--- BS_00/ep02/src/main.jsx
+++ BS_00/ep02/src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const user = {
+  name: "Biswajit Sahoo",
+  age: 26,
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App user={user} />
   </StrictMode>,
 )
